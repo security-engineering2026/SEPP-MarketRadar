1b9bb8c84186f8f867acbab19dba598a6d5c1627
security-engineering2026 <mypro.engineering@gmail.com>
2026-09-20 15:08:19 +0330

    Align Android Java and Kotlin JVM targets

--- android-companion/app/build.gradle.kts
+++ android-companion/app/build.gradle.kts
@@ -2,4 +2,11 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace="com.sepp.marketradar"; compileSdk=35
  defaultConfig { applicationId="com.sepp.marketradar"; minSdk=29; targetSdk=35; versionCode=16110; versionName="16.1.1" }
+ compileOptions {
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
+ }
+ kotlinOptions {
+  jvmTarget = "17"
+ }
 }
