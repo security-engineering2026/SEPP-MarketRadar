96800c526d7f4c6013bacbf2832b189ccd056b28
security-engineering2026 <mypro.engineering@gmail.com>
2026-09-22 08:31:06 +0330

    chore: synchronize Android companion version

--- android-companion/app/build.gradle.kts
+++ android-companion/app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace="com.sepp.marketradar"; compileSdk=35
- defaultConfig { applicationId="com.sepp.marketradar"; minSdk=29; targetSdk=35; versionCode=16110; versionName="16.1.1" }
+ defaultConfig { applicationId="com.sepp.marketradar"; minSdk=29; targetSdk=35; versionCode=16120; versionName="16.1.2" }
 }
